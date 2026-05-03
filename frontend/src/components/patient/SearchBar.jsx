64dd7d416372e65011dded984bceb0d5ddd1eeea
Nandita R Nadig <nanditanadig05@gmail.com>
2026-05-03 16:43:42 +0530

    Cleaner desc statements

--- frontend/src/components/patient/SearchBar.jsx
+++ frontend/src/components/patient/SearchBar.jsx
@@ -11,18 +11,9 @@ const SearchBar = () => {
     const [rerouteTo, setRerouteTo] = useState(false);
     const [nameofPerson, setNameofPerson] = useState("");
 
-    const [debouncedQuery, setDebouncedQuery] = useState("");
-
     const handleInputChange = (e) => {
         setInputChange(e.target.value);
     };
-
-    useEffect(() => {
-        const timeoutId = setTimeout(() => {
-            handleSearches(inputChange);
-        }, 300); // 300ms debounce
-        return () => clearTimeout(timeoutId);
-    }, [inputChange]);
 
     const handleSearches = async (value) => {
         if (value.trim()) {
