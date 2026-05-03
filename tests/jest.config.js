5287625f8b55d7557e4bd2201a4cc77daf6fee6b
Nandita R Nadig <nanditanadig05@gmail.com>
2026-05-03 16:18:50 +0530

    fix: edited to store binaries in node_modules

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -9,7 +9,5 @@ export default {
     '<rootDir>/unit/backend/**/*.test.js',
     '<rootDir>/integration/backend/**/*.test.js',
     '<rootDir>/backend/**/*.test.js'
-  ],
-  globalSetup: '<rootDir>/backend/globalSetup.js',
-  globalTeardown: '<rootDir>/backend/globalTeardown.js',
+  ]
 };
