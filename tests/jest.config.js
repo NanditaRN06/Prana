f906d17425faa0ad56f53298a2c84d234f790650
Nandita R Nadig <nanditanadig05@gmail.com>
2026-05-12 22:12:35 +0530

    feat: implemented and initialised test configuration

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -2,12 +2,34 @@ export default {
   testEnvironment: 'node',
   transform: {},
   moduleNameMapper: {
-    '^@backend/(.*)$': '<rootDir>/../backend/$1',
+    '^@backend/(.*)$': '<rootDir>/backend/$1',
+    '^mongoose$': '<rootDir>/backend/node_modules/mongoose',
   },
-  rootDir: '.',
+  rootDir: '..',
+  roots: ['<rootDir>/tests', '<rootDir>/backend'],
+  globalSetup: '<rootDir>/tests/setup/globalSetup.js',
+  globalTeardown: '<rootDir>/tests/setup/globalTeardown.js',
   testMatch: [
-    '<rootDir>/unit/backend/**/*.test.js',
-    '<rootDir>/integration/backend/**/*.test.js',
-    '<rootDir>/backend/**/*.test.js'
-  ]
+    '<rootDir>/tests/unit/backend/**/*.test.js',
+    '<rootDir>/tests/integration/backend/**/*.test.js',
+    '<rootDir>/tests/e2e/backend/**/*.test.js',
+    '<rootDir>/tests/system/backend/**/*.test.js'
+  ],
+  collectCoverageFrom: [
+    '<rootDir>/backend/**/*.js',
+    '!<rootDir>/backend/node_modules/**',
+    '!<rootDir>/backend/eslint.config.mjs',
+    '!<rootDir>/backend/templates/**'
+  ],
+  coverageDirectory: 'tests/coverage/backend',
+  coverageProvider: 'v8',
+  coverageReporters: ['text', 'lcov', 'clover'],
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 90,
+      lines: 90,
+      statements: 90
+    }
+  }
 };
